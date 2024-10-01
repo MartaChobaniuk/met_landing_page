ddd95465cf07f7bffe642c5e20b09e6d9cf51a6f
Marta Chobaniuk <marta.chobaniuk@gmail.com>
2024-10-01 19:23:58 +0300

    make build

---
+++ index.58b74ace.js
@@ -0,0 +1,2 @@
+window.addEventListener("hashchange",function(){"#menu"===window.location.hash?document.body.classList.add("main-page__body--with-menu"):document.body.classList.remove("main-page__body--with-menu")});var e=document.getElementById("form");e.addEventListener("submit",function(n){n.preventDefault(),e.reset()});
+//# sourceMappingURL=index.58b74ace.js.map
